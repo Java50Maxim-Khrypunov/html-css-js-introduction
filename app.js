96f3a5bcf0b60b63ea7840e5eba44f0cecb03a32
Maxim Khrypunov <khrypunovmax@gmail>
2023-01-04 14:20:28 +0200

    HOMEWORK#14

--- app.js
+++ app.js
@@ -1,34 +1,54 @@
-// const ar = [];
-// const ar1 = [1,2,3,4]
-// ar.push(...ar1, 2, "Hello");
-// ar.push ("abc");
+const array = [10, 20, 30, -10, 11, 100];
+const index = array.indexOf(3);
+console.log(index);
+// removind all numbers from 30
+// const res = array.splice(index);
+// array.splice(index,0,-10,-20);
+// array.splice(index,2,-20);
+// console.log(array);
+// const ar1 = [40,200];
+// array.splice(index, 1,...ar1);
+// array.splice(index, 1, ar1);
+// console.log(array);
+// const res = array.splice(index,1);
+// console.log(res);
+// const numbersPerPage = 2;
+// const page = 2;
+// const firstIndex = (page-1)*numbersPerPage;
+// const pageNumbers = array.slice(firstIndex, firstIndex+numbersPerPage);
+// const copy = array.slice(2,5);
+// console.log(copy);
+// const ar1 = array.map(multiply2);
+// function multiply2(number)
+// {
+//     return number * 2;
+// }
+// // console.log(ar1);
+// const ar2 = array.map(function (number, index)
+// {
+// return index + 1 + ". " + number;
+// });
+// console.log(ar2);
+// const ar3 = array.map(function(number)
+// {
+// return `<li>${number}</li>`
+// })
+// const ulElement = `<ul>${ar3.join('')}</ul>`;
+// console.log(ulElement);
+// Array.from(String);
 
-function getRandomNumber (min, max)
-{
-    min = Math.trunc(min);
-    max = Math.trunc(max);
-    return min + Math.trunc (Math.random() * (max - min + 1));
-}
+// const min = 5;
+// const max = 15;
+// const ar4 = array.filter(function(number)
+// {
+//     return number >= min && number <=max;
+// });
 
-function getRandomMatrix(rows, columns, min, max)
-{
-    const matrix = [];
-    for (let i = 0; i < rows; i++)
-    {
-        matrix.push([]);
-        for (let j = 0; j < columns; j++)
-        {
-            matrix[i].push(getRandomNumber(min, max));
-        }
-    }
-    return matrix;
-}
-const matrix = getRandomMatrix(3, 4, 0, 1,);
-console.log(matrix);
+// console.log(ar4);
 
-const strClass = getRandomNumber(0, 1) === 0 ? "white" : "black";
-const ar10 = [1,2,3,4,5];
-const str = ar10.join();
-const str3 = `class = "${strClass}"`;
-console.log(str3);
+// const ar5 = array.filter(function(number, index)
+// {
+//     return number% 2 === 0 && index%2 === 0;
+// });
 
+// console.log(ar5);

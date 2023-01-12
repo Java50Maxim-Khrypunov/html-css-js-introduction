2556a5b586142270133f79fb65bb0696f879e416
Maxim Khrypunov <khrypunovmax@gmail>
2023-01-12 15:59:45 +0200

    HomeWork#16

--- src/wordle.js
+++ src/wordle.js
@@ -2,16 +2,18 @@
 const guessInputElement = document.querySelector(".guess-input");
 const guessLettersElement = document.querySelector(".guess-letters");
 let lettersDivs;
-const trialsElement = document.querySelector(".trials-remained");
+let trialsElement = document.querySelector(".trials-remained");
 const gameResultElement = document.querySelector(".game-result");
 const playAgainElement = document.querySelector(".play-again");
 //variables required for JS logic
-const wordsForGuess = ["pappy", "apple", "table","gold","silver","index","script","peace","picture"];
+const wordsForGuess = ["pappy", "apple", "table","gold","silver","index","script","peace","picture", "car", "cat"];
 let trials;
 let word;
 let flGameOver = false;
 //Functions
-function startGame(){
+
+function startGame()
+{
     word = getword();
     fillLettersDivs();
     flGameOver = false;
@@ -20,11 +22,15 @@ function startGame(){
     gameResultElement.innerHTML = "";
     trialsElement.innerHTML = `remained trials ${trials}`;
 }
-function getword(){
+
+function getword()
+{
     const index = Math.trunc(Math.random() * wordsForGuess.length);
     return wordsForGuess[index];
 }
-function fillLettersDivs(){
+
+function fillLettersDivs()
+{
     const arrayWord = Array.from(word);
     guessLettersElement.innerHTML = arrayWord.reduce (function (res,cur){
         res = res + '<div class = "letter"></div>'
@@ -32,38 +38,62 @@ function fillLettersDivs(){
     },'')
     lettersDivs = document.querySelectorAll(".letter");
 }
-function onChange(){
-    if(flGameOver){
+
+function onChange()
+{
+    if(flGameOver)
+    {
         alert("Game is over, press play-again")
-    } else{
+    } 
+    else
+    {
         const trialWord = guessInputElement.value; 
         trials --;
-        trialsElement.innerHTML = `remained trials ${trials}`;
-        if (trialWord.length != word.length){
-            alert("wrong numbers of letters")
-        } else{
-            coloringWord(trialWord);
-        }
-    }
- 
-
-}
-function coloringWord(trialWord){
+        let counter;
+        counter++;
+        trialsElement.innerHTML = `REMAINED TRIALS ${trials}`;
+        if (trialWord.length != word.length)
+        {
+            alert("WRONG NUMBERS OF LETTERS")
+        } 
+        else {coloringWord(trialWord);  
+        let control =1 ;
+        if (trialWord == word)
+        {finishGame(control)};
+        if (trials === 0) {finishGame(0)};
+        }}
+    
+    function coloringWord(trialWord)
+    {
     const arrayWord = Array.from(trialWord);
-    arrayWord.forEach(function(letter, index){
+    arrayWord.forEach(function(letter, index)
+    {
         lettersDivs[index].innerHTML = letter;
         lettersDivs[index].style.color = getColor (letter,index);
-    });
-}
-function getColor(letter, index){
+    })};
+
+    function getColor(letter, index)
+    {
     let res = "red"
     if(word.includes(letter)){
         res = word[index] == letter ?  "green" : "yellow";
     }
-    return res;
+    return res;}
+
+function finishGame(controlNumber) {
+    if (controlNumber==1) {
+        gameResultElement.innerHTML =  "You are winner"
+        flGameOver = true;
+    playAgainElement.style.display='block';
+    trialsElement.innerHTML = 'You guessed the word';
+    } else if (controlNumber == 0) {
+        gameResultElement.innerHTML =  "You are loser";
+        flGameOver = true;
+    playAgainElement.style.display='block';
+    trialsElement.innerHTML = 'You do not guessed the word';
+    }
+   
+   
 }
-function finishGame(){
-    //TODO
 }
-//actions 
 startGame();

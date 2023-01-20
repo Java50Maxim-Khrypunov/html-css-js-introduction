16142c9baf2bedebe60216b66f133f9ff3f78849
Maxim Khrypunov <khrypunovmax@gmail>
2023-01-20 10:11:30 +0200

    HW_19

--- app.js
+++ app.js
@@ -1,22 +1,4 @@
-const employee1 = {"id": 123, name: "Gosha", birthYear: 2000, salary: 15000, 
-address: {city:"Lod", country: "Israel"}};
 
-const employee2 = {"id": 123, name: "Gosha", birthYear: 2000, salary: 15000, 
-address: {city:"Lod", country: "Israel"}};
-
-console.log(`employee1 === employee2 is ${employee1 == employee2}`);
-
-const employee3 = employee1;
-
-console.log(`employee3 === employee1 is ${employee3 == employee1}`);
-
-employee3.salary = 20000;
-console.log(`employee1 salary = ${employee1.salary}`);
-
-function createEmployee (id, name, birthYear, salary, city, country)
-{
-    return{id, name, birthYear, salary, address: {city, country}}
-}
 const employees = [
     createEmployee(12323, "Gosha", 1999, 15000, "Lod", "Israel"),
     createEmployee(12324, "David", 1975, 15500, "Tel-Aviv", "Israel"), 
